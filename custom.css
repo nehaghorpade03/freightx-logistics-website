b6b470a7b9cc84cb187b1d645c6efd17d60cdf60
Neha Ghorpade <neharghorpade@coep.sveri.ac.in>
2025-10-28 08:37:07 +0530

    Add files via upload

---
+++ custom.css
@@ -0,0 +1,41 @@
+.nav-link:hover{
+    border-bottom: 2px solid rgb(10, 95, 175);
+}
+header{
+    background:linear-gradient(rgba(24, 38, 46, 0.5),rgba(24, 38, 46,0.5)) , url(Banner.jpg);
+    background-size: cover;
+    background-position: center;
+    text-align: center;
+    color: white;
+    padding-top: 90px;
+    padding-bottom: 120px;
+}
+header img{
+    height: 80px;
+}
+#my-cards{
+    margin-top: -60px;
+}
+#my-cards h5{
+    margin-top: 10px;
+}
+
+#why-choose-us{
+    background:linear-gradient(rgba(24, 38, 46, 0.5),rgba(24, 38, 46,0.5)) , url(Banner1.jpg);
+    background-size: cover;
+    background-position: center;
+    text-align: center;
+    padding-top: 80px;
+    padding-bottom: 80px;
+}
+.xyz{
+    transition: 1s;
+}
+.xyz:hover{
+    transform: scale(1.2);
+}
+#top-button{
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+}
